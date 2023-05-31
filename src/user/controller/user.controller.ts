748303c81cfa10f04f4a6d5eda9ff5d0336a81e7
tanhun <tanhoang0710@gmail.com>
2023-05-31 15:59:08 +0700

    CRUD Users & TypeORM with Observables

---
+++ src/user/controller/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Observable<User> {
+    return this.userService.create(user);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Observable<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Get()
+  findAll(): Observable<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: number): Observable<any> {
+    return this.userService.deleteOne(id);
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: number, @Body() user: User): Observable<any> {
+    return this.userService.updateOne(id, user);
+  }
+}
